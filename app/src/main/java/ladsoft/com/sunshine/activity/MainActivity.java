e7fa77616125ab0890005d15caab16361e24942e
Cristhian Suematsu <suematsu01@gmail.com>
2016-11-20 15:52:43 -0200

    Create new package directories

--- app/src/main/java/ladsoft/com/sunshine/activity/MainActivity.java
+++ app/src/main/java/ladsoft/com/sunshine/activity/MainActivity.java
@@ -1,4 +1,4 @@
-package ladsoft.com.sunshine;
+package ladsoft.com.sunshine.activity;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import ladsoft.com.sunshine.R;
 
 public class MainActivity extends AppCompatActivity {
 
