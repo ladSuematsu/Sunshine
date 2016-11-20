bc72f1b14974b13ea6b5fed0b94baa808256b19a
Cristhian Suematsu <suematsu01@gmail.com>
2016-11-20 16:36:45 -0200

    Create collapsible toolbar layout for main fragment

--- app/src/main/java/ladsoft/com/sunshine/activity/MainActivity.java
+++ app/src/main/java/ladsoft/com/sunshine/activity/MainActivity.java
@@ -1,45 +1,30 @@
 package ladsoft.com.sunshine.activity;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import ladsoft.com.sunshine.R;
+import ladsoft.com.sunshine.databinding.ActivityMainBinding;
+import ladsoft.com.sunshine.fragment.ForecastFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-    }
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if(savedInstanceState == null) {
+            inflateFragment();
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    private void inflateFragment() {
+        getSupportFragmentManager().beginTransaction()
+            .replace(binding.content.getId(), ForecastFragment.newInstance())
+            .commit();
     }
 }
